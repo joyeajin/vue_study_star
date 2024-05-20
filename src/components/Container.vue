859dfa6b46dd482e9c7b19b19d4a7a2b5c2cb81c
joyeajin <dpwls31200@gmail.com>
2024-05-20 15:41:51 +0900

    first commit

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <!-- <Post
+      :instaDatas="instaDatas[i]"
+      v-for="(instaData, i) in instaDatas"
+      :key="i"
+    /> -->
+    <Post :instaDatas="instaDatas" />
+  </div>
+</template>
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container-a",
+  components: {
+    Post: Post,
+  },
+  props: {
+    instaDatas: Array,
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/Post.vue
@@ -0,0 +1,70 @@
+<template lang="">
+  <div class="post" v-for="(instaData, i) in instaDatas" :key="i">
+    <div class="post-header">
+      <div
+        class="profile"
+        :style="{ backgroundImage: `url(${instaDatas[i].userImage})` }"
+      ></div>
+      <span class="profile-name">{{ instaDatas[i].name }}</span>
+    </div>
+    <div
+      class="post-body"
+      :style="{ backgroundImage: `url(${instaDatas[i].postImage})` }"
+    ></div>
+    <div class="post-content">
+      <p>{{ instaDatas[i].likes }} Likes</p>
+      <p>
+        <strong>{{ instaDatas[i].name }}</strong> {{ instaDatas[i].content }}
+      </p>
+      <p class="date">{{ instaDatas[i].date }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Post-a",
+  props: {
+    instaDatas: Object,
+  },
+};
+</script>
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://picsum.photos/100?random=0");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://picsum.photos/600?random=0");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
